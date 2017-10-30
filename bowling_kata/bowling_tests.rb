2188efb7cbc03e9af60fcc33220653f07ed12ff1
hungariaia <hungariaia@gmail.com>
2017-10-30 17:51:26 +0100

    refactor

--- bowling_kata/bowling_tests.rb
+++ bowling_kata/bowling_tests.rb
@@ -3,18 +3,22 @@ require 'minitest/autorun'
 
 class BowlingTest < Minitest::Test
 
+
+   def setup
+     @game = Bowling.new
+   end
+
    def test_roll_in_a_frame
-     game = Bowling.new()
-     game.roll(3)
-     assert_equal(game.score,3)
+
+     @game.roll(3)
+     assert_equal(@game.score,3)
    end
 
    def test_two_roll
-     game = Bowling.new()
-     game.roll(3)
-     game.roll(5)
-     assert_equal(game.score,8)
-   end
 
+     @game.roll(3)
+     @game.roll(5)
+     assert_equal(@game.score,8)
+   end
 
 end
