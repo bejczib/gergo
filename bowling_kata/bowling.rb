b0a50f67d7476bd738a7352788c40370296c67ef
hungariaia <hungariaia@gmail.com>
2017-11-05 17:12:48 +0100

    deleted some old tests, added open and strke test, modified score method to handle strike,created delete_points method

--- bowling_kata/bowling.rb
+++ bowling_kata/bowling.rb
@@ -9,6 +9,21 @@ class Bowling
   end
 
   def score
-    @points.reduce(:+)
+    n = 1
+    while n < 20
+      if @points[n-1] == 10
+        POINTS << @points[n-1] + @points[n] + @points[n+1]
+      elsif @points[n-1] + @points[n] == 10 && @points[n] !=  0
+        POINTS << @points[n-1] + @points[n] + @points[n+1]
+      else
+        POINTS << @points[n-1] + @points[n]
+      end
+      n += 2
+    end
+    POINTS.reduce(:+)
+  end
+
+  def self.delet_points
+    POINTS.clear
   end
 end
--- bowling_kata/bowling_tests.rb
+++ bowling_kata/bowling_tests.rb
@@ -8,17 +8,50 @@ class BowlingTest < Minitest::Test
      @game = Bowling.new
    end
 
-   def test_roll_in_a_frame
-
-     @game.roll(3)
-     assert_equal(@game.score,3)
+   def multi_roll(c,p)
+    c.times { @game.roll(p) }
    end
 
-   def test_two_roll
 
-     @game.roll(3)
-     @game.roll(5)
-     assert_equal(@game.score,8)
-   end
+  #  def test_roll_in_a_frame
+   #
+  #    @game.roll(3)
+  #    assert_equal(@game.score,3)
+  #  end
+   #
+  #  def test_two_roll
+   #
+  #    @game.roll(3)
+  #    @game.roll(5)
+  #    assert_equal(@game.score,8)
+  #  end
+   #
+    def test_spare
+       @game.roll(4)
+       @game.roll(6)
+       @game.roll(3)
+       multi_roll(17,0)
+       assert_equal(16,@game.score)
+       Bowling.delet_points
+    end
+
+     def test_open
+        @game.roll(3)
+        @game.roll(5)
+        @game.roll(3)
+        multi_roll(17,0)
+        assert_equal(11,@game.score)
+        Bowling.delet_points
+     end
+
+    def test_strike
+       @game.roll(10)
+       @game.roll(6)
+       @game.roll(3)
+       multi_roll(17,0)
+       assert_equal(22,@game.score)
+       Bowling.delet_points
+    end
+
 
 end
